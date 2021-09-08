09968ac5cbff50d414afecdb601448c140508eea
PuneetGopinath <baalkrshna@gmail.com>
2021-09-08 18:07:36 +0530

    fix(index.js): fix previleged intent error

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const client = new Client({
     intents: [
         Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MEMBERS,
+        //Intents.FLAGS.GUILD_MEMBERS,
         Intents.FLAGS.GUILD_MESSAGES,
         Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
         Intents.FLAGS.DIRECT_MESSAGES,
