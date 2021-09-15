a7a43edc4e52f644689139acac7b5e9898b4ade8
PuneetGopinath <baalkrshna@gmail.com>
2021-09-15 10:20:45 +0530

    fix(bump reminder): switch to cron instead of node-cron

--- src/events/ready.js
+++ src/events/ready.js
@@ -2,6 +2,20 @@ module.exports = {
     name: "ready",
     once: true,
     async execute(client) {
+        const { CronJob } = require("cron");
+        const job = new CronJob(
+            "0 0 */2 * * *",
+            () => {
+                client.channels.cache
+                    .get("870330763772563482")
+                    .send(`<@&${client.config.roles.bumper}> Time to bump`);
+            },
+            null,
+            true,
+            "America/Los_Angeles"
+        );
+        job.start();
+
         const presences = [
             {
                 name: `devs who use ${client.config.prefix}helpme <text>`,
