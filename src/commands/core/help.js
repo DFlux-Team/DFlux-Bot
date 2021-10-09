a62d5f6940a4502261be914baeca6210352a4482
GNVageesh <vageeshgn2005@gmail.com>
2021-10-09 21:24:51 +0530

    Added Help Command

---
+++ src/commands/core/help.js
@@ -0,0 +1,36 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "help",
+    exceute: async ({ message }) => {
+        const helpEmbed = new MessageEmbed()
+            .setColor("RANDOM")
+            .setTitle("DFlux BOT")
+            .setURL("https://github.com/DFlux-Team/DFlux-Bot")
+            .setDescription(
+                "A BOT That Is Busy In Helping the People On the DevFlux Server"
+            )
+            .setThumbnail("https://i.imgur.com/NzuazOI.png")
+            .addFields(
+                {
+                    name: "🏓ping",
+                    value: "Returns a Pong message as well as the current stats about the BOT",
+                },
+                { name: "\u200B", value: "\u200B" },
+                {
+                    name: "❓quiz",
+                    value: "Runs a Quick Quiz to test your potenial",
+                },
+                { name: "\u200B", value: "\u200B" },
+                { name: "🤚help", value: "Provides this Help Message" },
+                {
+                    name: "🙋helpme",
+                    value: "Pings a User who can you help you with the problem your facing",
+                }
+            )
+            .setTimestamp()
+            .setFooter(`Help Command Issused by ${message.author} `);
+
+        message.channel.send({ embed: [helpEmbed] });
+    },
+};
